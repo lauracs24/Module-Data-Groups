2fbe1928f29eb40f7ba5786908fe6911f7c1e116
Laura C <laura.csaki.uk@gmail.com>
2026-03-03 12:47:46 +0000

    Add even median case and no-mutation test

--- prep/median.js
+++ prep/median.js
@@ -1,6 +1,21 @@
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  return list[middleIndex];
+  const length = list.length;
+
+  if (length === 0) {
+    throw new Error("Cannot calculate median of empty array");
+  }
+
+  const middleIndex = Math.floor(length / 2);
+
+  // Odd length: middle element
+  if (length % 2 === 1) {
+    return list[middleIndex];
+  }
+
+  // Even length: average of the two middle elements
+  const lowerMiddle = list[middleIndex - 1];
+  const upperMiddle = list[middleIndex];
+  return (lowerMiddle + upperMiddle) / 2;
 }
 
 module.exports = calculateMedian;
--- prep/median.test.js
+++ prep/median.test.js
@@ -7,3 +7,18 @@ test("calculates the median of a list of odd length", () => {
 
   expect(currentOutput).toEqual(targetOutput);
 });
+
+test("doesn't modify the input", () => {
+  const list = [1, 2, 3];
+  calculateMedian(list);
+
+  expect(list).toEqual([1, 2, 3]);
+});
+
+test("calculates the median of a list of even length", () => {
+  const list = [10, 20, 30, 40]; // already sorted
+  const currentOutput = calculateMedian(list);
+  const targetOutput = 25; // (20 + 30) / 2
+
+  expect(currentOutput).toEqual(targetOutput);
+});
